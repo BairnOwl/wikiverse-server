a0de072bce0968b2b5d85bc113c4eb0c9036d7ee
Linda Chang <linda_chang@brown.edu>
2016-10-21 22:00:28 -0400

    added http request

--- server.js
+++ server.js
@@ -4,6 +4,24 @@ var app = express();
 var http = require('http');
 var server = http.createServer(app);
 
+app.get('/', function(request, response){
+	req = new XMLHttpRequest();
+
+	url = '';
+
+	req.open('GET', url, true);
+
+	req.addEventListener('load', function(e){
+		if (req.status == 200) {
+			var data = JSON.parse(req.responseText);
+			response.json(data);
+		}
+	}, false);
+
+	req.send(null); 
+});
+
+
 server.listen(process.env.PORT, function () {
     console.log('wikiverse app listening on port ' + process.env.PORT);
 });
