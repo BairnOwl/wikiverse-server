d4ff8dcfb64971b39383f6f59130dc31d3614a42
Nina <Nina>
2016-10-21 23:33:19 -0400

    Stuff from media wiki...

--- server.js
+++ server.js
@@ -25,7 +25,22 @@ app.get('/query', function(request,response) {
 	req = new XMLHttpRequest();
 
 	//base url:
-	var base = http://en.wikipedia.org/w/api.php?action=query&format=json&prop=links%7Cimages%7Cextracts&titles=Albert+Einstein&pllimit=500&exintro=1&explaintext=1&exsectionformat=plain
+	var base = 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=links%7Cimages%7Cextracts&titles=';
+	var title = 'Albert Einstein';
+	url = base + title + '&pllimit=500&exintro=1&explaintext=1&exsectionformat=plain';
+
+	req.open('GET', url, true);
+
+	req.addEventListener('load', function(e){
+		if (req.status == 200) {
+			var data = JSON.parse(req.responseText);
+			var title = data.title;
+			var links = data.links;
+			var text = data.extract;
+			var images = data.images;
+			response.json(title, links, text, images);
+		}
+	}, false);
 
 	req.send(null); 
 });
