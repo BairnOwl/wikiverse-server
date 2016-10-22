17284711ec74d5d76cd6538c52fbbc2a340306a8
Nina <Nina>
2016-10-21 23:16:03 -0400

    Waaa

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var app = express();
 var http = require('http');
 var server = http.createServer(app);
 
-app.get('/', function(request, response){
+app.get('/wiki', function(request, response){
 	req = new XMLHttpRequest();
 
-	url = '';
+	url = 'http://en.wikipedia.org/w/api.php?action=query&format=json&prop=links%7Cimages%7Cextracts&titles=Albert+Einstein&pllimit=500&exintro=1&explaintext=1&exsectionformat=plain';
 
 	req.open('GET', url, true);
 
@@ -21,6 +21,14 @@ app.get('/', function(request, response){
 	req.send(null); 
 });
 
+app.get('/query', function(request,response) {
+	req = new XMLHttpRequest();
+
+	//base url:
+	var base = http://en.wikipedia.org/w/api.php?action=query&format=json&prop=links%7Cimages%7Cextracts&titles=Albert+Einstein&pllimit=500&exintro=1&explaintext=1&exsectionformat=plain
+
+	req.send(null); 
+});
 
 server.listen(process.env.PORT, function () {
     console.log('wikiverse app listening on port ' + process.env.PORT);
